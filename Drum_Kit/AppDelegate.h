3536a759dccba821dcea3ba52817817eed2e53af
Love <Love@love.local>
2016-09-29 00:01:12 +0530

    Initial Commit

---
+++ Drum_Kit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Drum_Kit
+//
+//  Created by Love on 9/29/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Drum_Kit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Drum_Kit
+//
+//  Created by Love on 9/29/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
